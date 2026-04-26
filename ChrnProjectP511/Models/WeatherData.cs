6ac192fd65b49c93b0d4c5c0d8eea44164ecd66d
Serg <vs@mail.ru>
2026-04-26 13:35:59 +0500

    добавил модели в WeatherData

--- ChrnProjectP511/Models/WeatherData.cs
+++ ChrnProjectP511/Models/WeatherData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,13 @@ namespace ChrnProjectP511.Models
 {
     public class WeatherData
     {
-
+        public string CityName { get; set; }
+        public double Temperature { get; set; }
+        public double WindSpeed { get; set; }
+        public int Huidity { get; set; }
+        public int Pressure { get; set; }
+        public string WeatherCode { get; set; }
+        public string WeatherDiscription { get; set; }
+        public string LastUpdated { get; set; }
     }
 }
