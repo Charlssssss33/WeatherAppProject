f6baf24f01f56bbb413b2ece6efc3dd4371c497b
Serg <vs@mail.ru>
2026-04-26 14:52:01 +0500

    добавил метод который превращает числовой код погоды в понятный тест

---
+++ ChrnProjectP511/Models/WeatherCodeMapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChrnProjectP511.Models
+{
+    public static class WeatherCodeMapper
+    {
+        public static string GetDescription(int code)
+        {
+            return code switch
+            {
+                0 => "Ясно",
+                1 => "В основном Ясно",
+                2 => "Переменная облачность",
+                3 => "Пасмурно",
+                45 => "Туман",
+                51 => "Морось",
+                61 => "Дождь",
+                71 => "Снег",
+                80 => "Ливень",
+                _ => "Неизвестно"
+            };
+        }
+        
+
+        
+    }
+}
