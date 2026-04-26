bd469f63d559060d978d6b70a451e64bf5436e17
Serg <vs@mail.ru>
2026-04-26 14:32:10 +0500

    полностью заполнил CurrentWeather и WeatherApiResponse

---
+++ ChrnProjectP511/Models/CurrentWeather.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ChrnProjectP511.Models
+{
+    public class CurrentWeather
+    {
+        [JsonProperty("temperature")]
+        public double Temperature { get; set; }
+
+        [JsonProperty("windspeed")]
+        public double WindSpeed {  get; set; }
+
+        [JsonProperty("weathercode")]
+        public int WeatherCode { get; set; }
+
+        [JsonProperty("time")]
+        public string Time {  get; set; }
+    }
+}
---
+++ ChrnProjectP511/Models/WeatherApiResponse.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ChrnProjectP511.Models
+{
+    public class WeatherApiResponse
+    {
+        [JsonProperty("current_weather")]
+        public CurrentWeather CurrentWeather { get; set; } = new();
+
+        [JsonProperty("hourlydata")]
+        public HourlyData Hourly { get; set; } = new();
+    }
+}
