c1de49e52cfed71fcabccde00f131fd57237ce85
Serg <vs@mail.ru>
2026-04-26 14:09:09 +0500

    созданы классы для ответов JSON

---
+++ ChrnProjectP511/Models/GeocodingResults.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChrnProjectP511.Models
+{
+    public class GeocodingResults
+    {
+        [JsonProperty("name")]
+     public string Name { get; set; }
+    }
+}
